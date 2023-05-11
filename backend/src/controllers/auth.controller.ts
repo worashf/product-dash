683cc984671f5326e74a0e6f8cb43cecfb7cea4d
Worash <uppert83@gmail.com>
2023-05-11 14:49:41 +0300

    Implement user registration

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,20 +1,45 @@
 import { Request, Response } from "express";
+import * as bcrypt from "bcryptjs";
 import { registrationValidation } from "../validations/auth.validation";
-export const register = (req: Request, res: Response) => {
+import { saveUser, findUser } from "../repositories/user.repository";
+
+export const register = async (req: Request, res: Response) => {
   const { error } = registrationValidation.validate(req.body);
-  if (error) {
-    res.status(400).json({
-      error: error.details,
-      success: false,
-    });
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+  try {
+    if (password !== confirmPassword) {
+      res.status(400).json({
+        message: "Password do not match!",
+        success: false,
+      });
+    }
+    const existingUser = await findUser(email);
+    if (existingUser) {
+      res.status(400).json({
+        message: "Email already taken",
+      });
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const user = await saveUser({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hashPassword,
+      });
+
+      res.status(201).json({
+        user,
+        success: true,
+      });
+    }
+
+    if (error) {
+      res.status(400).json({
+        error: error.details,
+        success: false,
+      });
+    }
+  } catch (err) {
+    throw new Error(err);
   }
-  if (req.body.password !== req.body.confirmPassword) {
-    res.status(400).json({
-      message: "Password do not match!",
-      success: false,
-    });
-  }
-  res.status(200).json({
-    value: req.body,
-  });
 };

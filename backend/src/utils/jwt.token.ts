ee9afbee6e19d271ea620b7e68d408d733bd4fe1
Worash <uppert83@gmail.com>
2023-05-12 13:30:48 +0300

    Add  jwt  token verify method

--- backend/src/utils/jwt.token.ts
+++ backend/src/utils/jwt.token.ts
@@ -1,4 +1,4 @@
-import { sign } from "jsonwebtoken";
+import { sign, verify, JwtPayload } from "jsonwebtoken";
 import { config } from "dotenv";
 
 // load enviroment variables
@@ -6,6 +6,14 @@ config();
 export const generateJwtToken = (payload: object) => {
   try {
     return sign(payload, process.env.JWT_SECRET);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const verifyToken = (token: string) => {
+  try {
+    return verify(token, process.env.JWT_SECRET) as JwtPayload;
   } catch (err) {
     console.log(err);
   }

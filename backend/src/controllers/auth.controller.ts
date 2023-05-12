06472810aa89305b85a5ee059c7688f055da52e5
Worash <uppert83@gmail.com>
2023-05-12 11:08:19 +0300

    Add  token in cookie

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,45 +1,94 @@
 import { Request, Response } from "express";
 import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
 import { registrationValidation } from "../validations/auth.validation";
 import { saveUser, findUser } from "../repositories/user.repository";
-
+import { generateJwtToken } from "../utils/jwt.token";
 export const register = async (req: Request, res: Response) => {
-  const { error } = registrationValidation.validate(req.body);
   const { firstName, lastName, email, password, confirmPassword } = req.body;
+
   try {
-    if (password !== confirmPassword) {
-      res.status(400).json({
-        message: "Password do not match!",
-        success: false,
-      });
-    }
-    const existingUser = await findUser(email);
-    if (existingUser) {
-      res.status(400).json({
-        message: "Email already taken",
-      });
-    } else {
-      const hashPassword = await bcrypt.hash(password, 10);
-      const user = await saveUser({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: hashPassword,
-      });
-
-      res.status(201).json({
-        user,
-        success: true,
-      });
-    }
-
+    const { error } = registrationValidation.validate({
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+    });
     if (error) {
       res.status(400).json({
         error: error.details,
         success: false,
       });
+    } else {
+      if (password !== confirmPassword) {
+        res.status(400).json({
+          message: "Password do not match!",
+          success: false,
+        });
+      }
+      const existingUser = await findUser(email);
+      if (existingUser) {
+        res.status(400).json({
+          message: "Email already taken",
+        });
+      } else {
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await saveUser({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: hashPassword,
+        });
+
+        res.status(201).json({
+          user,
+          success: true,
+        });
+      }
     }
   } catch (err) {
-    throw new Error(err);
+    res.status(400).json({
+      error: err,
+      success: false,
+    });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await findUser(email);
+  if (!user) {
+    res.status(400).json({
+      success: false,
+      message: "Invalid credentials, user not found",
+    });
+  } else {
+    bcrypt
+      .compare(password, user.password)
+      .then((result: any) => {
+        if (result) {
+          const token = generateJwtToken({ id: user.id });
+          res
+            .status(200)
+            .cookie("token", token, {
+              maxAge: 24 * 60 * 60 * 1000, // 1day
+            })
+            .json({
+              success: true,
+              user,
+              token,
+            });
+        } else {
+          res.status(400).json({
+            success: false,
+            message: "Invalid credentials, password not matched!",
+          });
+        }
+      })
+      .catch((err: any) => {
+        throw new Error(err);
+      });
   }
 };

b93b52499a11eddb53a99dd14c427d5c6c809c0d
Worash <uppert83@gmail.com>
2023-05-12 13:28:50 +0300

    Remove debug codebase

--- backend/src/configs/orm.config.ts
+++ backend/src/configs/orm.config.ts
@@ -3,13 +3,7 @@ import dotenv from "dotenv";
 
 import User from "../entities/user.entity";
 dotenv.config();
-console.log(
-  process.env.DB_HOST,
-  process.env.DB_PORT,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  process.env.DB_NAME
-);
+
 const connectDB = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
